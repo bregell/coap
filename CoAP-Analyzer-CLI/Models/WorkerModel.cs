897d53e7047a78975b83d8a09ff7ab484b24e38e
Johan Bregell <johan_bregell@hotmail.com>
2015-04-14 18:35:37 +0200

    More stuff.

---
+++ CoAP-Analyzer-CLI/Models/WorkerModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net;
+
+namespace CoAP_Analyzer_Client.Models
+{
+    public class WorkerModel : INotifyPropertyChanged
+    {
+        #region Members
+        Worker _worker;
+        #endregion
+
+        #region Properties
+        public Worker Worker
+        {
+            get
+            {
+                return _worker;
+            }
+            set
+            {
+                _worker = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _worker.MethodToRun.Method.Name.ToString();
+            }
+        }
+
+        public IPAddress IP
+        {
+            get
+            {
+                return _worker.Host.IP;
+            }
+        }
+
+        public int Rate
+        {
+            get
+            {
+                return _worker.Rate;
+            }
+        }
+
+        public ObservableCollection<MeasureModel> Measure
+        {
+            get
+            {
+                return _worker.Measures;
+            }
+        }
+        #endregion
+
+        #region Construction
+        public WorkerModel(Host host, Func<int, Measure> f, int r, int param)
+        {
+            _worker = new Worker(host, f, r, param);
+        }
+        #endregion
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Methods
+        private void RaisePropertyChanged(string propertyName)
+        {
+            // take a copy to prevent thread issues
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+
+    public class WorkerListModel
+    {
+        #region Members
+        ObservableCollection<WorkerModel> _workers;
+        #endregion
+
+        #region Construction
+        public WorkerListModel()
+        {
+            _workers = new ObservableCollection<WorkerModel>();
+        }
+        #endregion
+
+        #region Properties
+        public ObservableCollection<WorkerModel> Workers
+        {
+            get
+            {
+                return _workers;
+            }
+            set
+            {
+                _workers = value;
+            }
+        }
+        #endregion
+    }
+}
---
+++ CoAp-Analyzer-GUI/UserControls/WorkerList.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CoAP_Analyzer_Client.Models;
+
+namespace CoAP_Analyzer_GUI.UserControls
+{
+    /// <summary>
+    /// Interaction logic for WorkerList.xaml
+    /// </summary>
+    public partial class WorkerList : UserControl
+    {
+        public WorkerList()
+        {
+            InitializeComponent();
+            this.DataContext = SharedData._workerList;
+        }
+
+        private void ListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            WorkerModel w = (WorkerModel)(workerlist.SelectedItem);
+            SharedData._measureList.Measures = w.Worker.Measures;
+        }
+    }
+}
